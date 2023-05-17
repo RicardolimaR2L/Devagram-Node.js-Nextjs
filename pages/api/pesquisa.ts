b36aecabd5c3c111cd9f1d47e80167c432a5f15e
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-05-17 10:47:28 -0300

    Adicionar a pesquisa por ID

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -11,25 +11,35 @@ const pesquisaEndpoint = async (
   try {
     if (req.method === 'GET') {
       //valida se o metodo da requisição é ou nao Get(usado para trazer dados)
-      const { filtro } = req.query
-      if (!filtro || filtro.length < 2) {
-        //valida se a busca tem no minimo dois caracteres
-        return res
-          .status(400)
-          .json({ erro: 'Favor informar pelo menos 2 caracteres para a busca' })
-      }
-      const usuariosEncontrados = await UsuarioModel.find({
-        $or: [
-          { nome: { $regex: filtro, $options: 'i' } }
-          //{ email: { $regex:filtro, $options: 'i' } }
-          /*  $regex é um operador que permite fazer correspondência de padrões usando expressões regulares.
-              filtro é a variável que contém o padrão a ser pesquisado.
-              $options: 'i' é uma opção que torna a busca case-insensitive, ou seja, não diferencia maiúsculas de minúsculas.
-          */
-        ]
-      })
+      if (req?.query?.id) {//verifica se na requisição tem o id do usuario
+        const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)// busaca o usuario no banco pelo id e  retorna o usuario encontrado 
+        if(!usuarioEncontrado){
+          return res.status(400).json({erro: 'usuario nao encontrado'})
+        }
+        usuarioEncontrado.senha = null//trataiva para nao mostrar a senha do usuario quando ele for encontrado
+        return res.status(200).json(usuarioEncontrado)
 
-      return res.status(200).json(usuariosEncontrados)
+      } else {
+        const { filtro } = req.query
+        if (!filtro || filtro.length < 2) {
+          //valida se a busca tem no minimo dois caracteres
+          return res.status(400).json({
+            erro: 'Favor informar pelo menos 2 caracteres para a busca'
+          })
+        }
+        const usuariosEncontrados = await UsuarioModel.find({
+          $or: [
+            { nome: { $regex: filtro, $options: 'i' } }
+            //{ email: { $regex:filtro, $options: 'i' } }
+            /*  $regex é um operador que permite fazer correspondência de padrões usando expressões regulares.
+                filtro é a variável que contém o padrão a ser pesquisado.
+                $options: 'i' é uma opção que torna a busca case-insensitive, ou seja, não diferencia maiúsculas de minúsculas.
+            */
+          ]
+        })
+
+        return res.status(200).json(usuariosEncontrados)
+      }
     }
     return res.status(405).json({ erro: 'metodo informado nao e valido' })
   } catch (e) {
